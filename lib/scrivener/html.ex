e5a71f7fe6ca7d2ef9ce8d85d05d616989952209
Lukasz D <lukasz.dacz@llinformatics.com>
2024-07-15 13:24:19 +0200

    change phoenix_html to phoenix_html_helpers

--- lib/scrivener/html.ex
+++ lib/scrivener/html.ex
@@ -1,6 +1,5 @@
 defmodule Scrivener.HTML do
   import Phoenix.HTML
-  import Phoenix.HTML.Form
   use PhoenixHTMLHelpers
   @defaults [view_style: :bootstrap, action: :index, page_param: :page, hide_single: false]
   @view_styles [:bootstrap, :semantic, :foundation, :bootstrap_v4, :materialize, :bulma]
